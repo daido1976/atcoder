5e6f075b417e41abd96cceec80e145d8e06cf4cc
Daido Shota <daido1976@gmail.com>
2021-02-07 17:10:50 +0900

    Resolve b_polygon with c++

--- cpp/b_polygon.cpp
+++ cpp/b_polygon.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 using namespace std;
 
+bool is_drawable(int max, int rem_sum)
+{
+  if (max < rem_sum)
+  {
+    return true;
+  }
+  return false;
+}
+
 int main()
 {
-  cout << to_string(1 + 1) + "\n";
-  cout << "Hello world!\n";
-  cout << "Hello world!" << endl;
+  string result;
+  int n;
+  cin >> n;
 
-  printf("Hello world!\n");
+  int maxL = 0, sum = 0;
+
+  for (int i = 0; i < n; ++i)
+  {
+    int L;
+    cin >> L;
+    sum += L;
+    maxL = max(maxL, L);
+  }
+
+  int rem_sum = sum - maxL;
+
+  if (is_drawable(maxL, rem_sum))
+  {
+    result = "Yes";
+  }
+  else
+  {
+    result = "No";
+  }
+
+  cout << result << endl;
 }
